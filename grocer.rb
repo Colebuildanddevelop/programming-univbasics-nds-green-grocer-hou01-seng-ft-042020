ae7622ad2683eeac6a5b4ad4ba3926fe59073594
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-14 02:52:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,20 +1,51 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  index = 0
+  hash = {}
+  while collection[index] do 
+    if name == collection[index][:item]
+      return hash = collection[index]
+    end
+    index += 1
+  end
+  nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  array_unique = []
+  index = 0
+  while cart[index] do 
+    item_hash = find_item_by_name_in_collection(cart[index][:item], cart)
+    if item_hash[:count]
+      item_hash[:count] += 1
+    else
+      item_hash[:count] = 1
+    end
+    array_unique << item_hash
+    index += 1
+  end
+  array_unique
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  updated_array = []
+  index = 0
+  while coupons[index] do 
+    item_name = coupons[index][:name]
+    if (coupons[index][:num] < item_in_cart[:count])
+      item_in_cart[:count] -= coupons[index][:num]
+      coupon_hash = {
+        
+      }
+    end
+    index += 1
+  end
+  
+  
+  updated_array
 end
 
 def apply_clearance(cart)
@@ -34,3 +65,10 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
+
+t = [
+  { :item => "DOG FOOD" },
+  { :item => "WINE" },
+  { :item => "STRYCHNINE" }
+]
+
